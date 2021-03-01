0c18b5daf11be7e14c0902cea0cf1a3043ab612e
Christopher Mortimer <christopher@mortimer.xyz>
2021-03-01 16:57:01 +1100

    adding another WDC testing geojson

---
+++ tableau-wdc/js/geo_earthquake_4.5_week.js
@@ -0,0 +1,88 @@
+(function () {
+  var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function (schemaCallback) {
+    var cols = [
+      {
+        id: "ID",
+        alias: "ID",
+        dataType: tableau.dataTypeEnum.string,
+        columnRole: tableau.columnRoleEnum.dimension,
+        columnType: tableau.columnTypeEnum.discrete,
+      },
+      {
+        id: "mag",
+        alias: "Magnitude",
+        dataType: tableau.dataTypeEnum.float,
+        columnRole: tableau.columnRoleEnum.measure,
+        columnType: tableau.columnTypeEnum.continuous,
+      },
+      {
+        id: "title",
+        alias: "Title",
+        dataType: tableau.dataTypeEnum.string,
+        columnRole: tableau.columnRoleEnum.dimension,
+        columnType: tableau.columnTypeEnum.discrete,
+      },
+      {
+        id: "geometry",
+        alias: "geometry",
+        dataType: tableau.dataTypeEnum.geometry,
+        columnRole: tableau.columnRoleEnum.dimension,
+        columnType: tableau.columnTypeEnum.discrete,
+      },
+    ];
+
+    var tableSchema = {
+      id: "geo_earthquake_4.5_week",
+      alias: "GEO EarthQuake 4.5 week",
+      columns: cols,
+    };
+
+    schemaCallback([tableSchema]);
+  };
+
+  myConnector.getData = function (table, doneCallback) {
+    loadJSON("geo_earthquake_4.5_week", function (resp) {
+      var respJson = JSON.parse(resp);
+      var dataarray = respJson,
+        tableData = [];
+
+      // Iterate over the JSON object
+      for (var i = 0, len = dataarray.length; i < len; i++) {
+        tableData.push({
+          ID: dataarray.features[i].properties.ID,
+          mag: dataarray.features[i].properties.mag,
+          title: dataarray.features[i].properties.title,
+          geometry: dataarray.features[i].geometry,
+        });
+      }
+
+      table.appendRows(tableData);
+      doneCallback();
+    });
+  };
+
+  tableau.registerConnector(myConnector);
+})();
+
+$(document).ready(function () {
+  $("#submitButton").click(function () {
+    tableau.connectionName = "GEO EarthQuake 4.5 week";
+    tableau.submit();
+  });
+});
+
+// Helper function that loads a json and a callback to call once that file is loaded
+function loadJSON(path, cb) {
+  var obj = new XMLHttpRequest();
+  obj.overrideMimeType("application/json");
+  obj.open("GET", "../json/" + path + ".json", true);
+  obj.onreadystatechange = function () {
+    if (obj.readyState == 4 && obj.status == "200") {
+      console.log(obj.responseText);
+      cb(obj.responseText);
+    }
+  };
+  obj.send(null);
+}
